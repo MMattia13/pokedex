77c0a9da16f36a23c3b2249515c9c44f391a8dff
MMattia13 <marianomattia14@gmail.com>
2025-01-07 16:37:18 +0100

    fix merge

--- backend/demo/src/main/java/com/pokedex/App.java
+++ backend/demo/src/main/java/com/pokedex/App.java
@@ -1,23 +1,15 @@
 package com.pokedex;
 
-<<<<<<< HEAD
 import com.pokedex.utility.DatabaseConnection;
 
 import io.javalin.Javalin;
 
 import static io.javalin.Javalin.create;
 
-=======
-/**
- * Hello world!
- *
- */
->>>>>>> 32dddd9cac8f9276c021d44932115da3eb7637b1
 public class App 
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         Javalin app = create(config -> {config.plugins.enableCors
                 (cors -> cors.add(it -> it.anyHost()));})
                 .start(8082);
@@ -28,8 +20,4 @@ public class App
 
 // StudentController studentController = new StudentController();
 // studentController.registerRoutes(app);
-=======
-        System.out.println( "Hello World!" );
-    }
-}
->>>>>>> 32dddd9cac8f9276c021d44932115da3eb7637b1
+
